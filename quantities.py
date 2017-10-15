9d59601eafe33cf95fca4f00c0a60d5db770238b
Kaleidophon <Dennis.Ulmer@gmx.de>
2017-10-15 16:17:51 +0200

    Introduced immutable quantities.

---
+++ quantities.py
@@ -0,0 +1,83 @@
+
+QUANTITY_SPACE_INFLOW = ("0", "+")
+QUANTITY_SPACE_OUTFLOW = QUANTITY_SPACE_VOLUME = ("0", "+", "max")
+
+QUANTITY_SPACES = {
+    "inflow": QUANTITY_SPACE_INFLOW,
+    "outflow": QUANTITY_SPACE_OUTFLOW,
+    "volume": QUANTITY_SPACE_VOLUME
+}
+
+
+class Quantifiable:
+    """
+    Class to model a magnitude or a derivative.
+    """
+
+    def __init__(self, value, quantity_space):
+        self.value = value
+        self.quantity_space = quantity_space
+        self.space_ceil = len(quantity_space) - 1
+        self.value_index = quantity_space.index(value)
+
+    def __add__(self, other):
+        assert other == 1, "You can only add one to a quantifiable."
+
+        if self.value_index != self.space_ceil:
+            self.value_index += 1
+            self.value = self.quantity_space[self.value_index]
+
+        return self
+
+    def __iadd__(self, other):
+        return self.__add__(other)
+
+    def __sub__(self, other):
+        assert other == 1, "You can only subtract one to a quantifiable."
+        if self.value_index != 0:
+            self.value_index -= 1
+            self.value = self.quantity_space[self.value_index]
+
+        return self
+
+    def __isub__(self, other):
+        return self.__sub__(other)
+
+    def __str__(self):
+        return self.value
+
+
+class FrozenQuantifiable(Quantifiable):
+
+    def __setattr__(self, key, value):
+        if key == "value":
+            raise KeyError("Cannot set value for an immutable quantity.")
+        super().__setattr__(key, value)
+
+
+class Quantity:
+    """
+    Class modeling a quantity of a inflow, outflow or volume.
+    """
+    magnitude = None
+    derivative = None
+
+    def __init__(self, model, magnitude="0", derivative="0"):
+        assert model in QUANTITY_SPACES.keys(), "Unknown model"
+        self.quantity_space = QUANTITY_SPACES[model]
+        assert magnitude in self.quantity_space, "Invalid value for magnitude: {}".format(magnitude)
+        assert derivative in self.quantity_space, "Invalid value for derivative: {}".format(derivative)
+        self.init_quantifiables(magnitude, derivative)
+
+    def init_quantifiables(self, magnitude, derivative):
+        # Wrap magnitude and derivative in Quantifiables for neat addition / subtraction functionalities
+        self.magnitude = Quantifiable(value=magnitude, quantity_space=self.quantity_space)
+        self.derivative = Quantifiable(value=derivative, quantity_space=self.quantity_space)
+
+
+class FrozenQuantity(Quantity):
+    def init_quantifiables(self, magnitude, derivative):
+        # Wrap magnitude and derivative in Quantifiables for neat addition / subtraction functionalities
+        self.magnitude = FrozenQuantifiable(value=magnitude, quantity_space=self.quantity_space)
+        self.derivative = FrozenQuantifiable(value=derivative, quantity_space=self.quantity_space)
+
--- transitions.py
+++ transitions.py
@@ -4,72 +4,17 @@ Module defining the transition graph.
 """
 
 # CONST
-QUANTITY_SPACE_INFLOW = ("0", "+")
-QUANTITY_SPACE_OUTFLOW = QUANTITY_SPACE_VOLUME = ("0", "+", "max")
-QUANTITY_SPACES = {
-    "inflow": QUANTITY_SPACE_INFLOW,
-    "outflow": QUANTITY_SPACE_OUTFLOW,
-    "volume": QUANTITY_SPACE_VOLUME
-}
-
-
-class Quantity:
-    """
-    Class modeling a quantity of a inflow, outflow or volume.
-    """
-    class Quantifiable:
-        """
-        Class to model a magnitude or a derivative.
-        """
-        def __init__(self, value, quantity_space):
-            self.value = value
-            self.quantity_space = quantity_space
-            self.space_ceil = len(quantity_space) - 1
-            self.value_index = quantity_space.index(value)
-
-        def __add__(self, other):
-            assert other == 1, "You can only add one to a quantifiable."
-
-            if self.value_index != self.space_ceil:
-                self.value_index += 1
-                self.value = self.quantity_space[self.value_index]
-
-            return self
-
-        def __iadd__(self, other):
-            return self.__add__(other)
-
-        def __sub__(self, other):
-            assert other == 1, "You can only subtract one to a quantifiable."
-            if self.value_index != 0:
-                self.value_index -= 1
-                self.value = self.quantity_space[self.value_index]
-
-            return self
-
-        def __isub__(self, other):
-            return self.__sub__(other)
-
-        def __str__(self):
-            return self.value
-
-    def __init__(self, model, magnitude="0", derivative="0"):
-        assert model in QUANTITY_SPACES.keys()
-        self.quantity_space = QUANTITY_SPACES[model]
-        assert magnitude in self.quantity_space
-        assert derivative in self.quantity_space
-
-        # Wrap magnitude and derivative in Quantifiables for neat addition / subtraction functionalities
-        self.magnitude = self.Quantifiable(value=magnitude, quantity_space=self.quantity_space)
-        self.derivative = self.Quantifiable(value=derivative, quantity_space=self.quantity_space)
-
+from quantities import Quantity, FrozenQuantity
 
 class State:
     """
     Class modeling the state in a transition graph.
     """
     def __init__(self, *quantities):
-        assert all([type(quantity) == Quantity for quantity in quantities])
+        assert all([type(quantity) == Quantity for quantity in quantities]), \
+            "All quantities of a state have to be of type Quantity: {} found".format(
+                " ,".join([str(type(arg)) for arg in quantities if type(arg) != Quantity])
+            )
         self.inflow_quantity, self.volume_quantity, self.outflow_quantity = quantities
 
 
@@ -90,11 +35,10 @@ def check_transition_graph_consistency(transition_graph):
 if __name__ == "__main__":
     outflow = Quantity("outflow", derivative="max")
     print("Magnitude", outflow.magnitude)
-    outflow.magnitude += 1
     print("Magnitude", outflow.magnitude)
     outflow.magnitude += 1
     print("Magnitude", outflow.magnitude)
-    outflow.magnitude += 1s
+    outflow.magnitude += 1
     print("Magnitude", outflow.magnitude)
     print("Derivative", outflow.derivative)
     outflow.derivative -= 1
