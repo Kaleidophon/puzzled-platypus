39b87305722276682a9a59f17e26fbacd16692ec
Gururaj Rao <gururajraop@gmail.com>
2017-10-19 22:33:03 +0200

    Some silly tryouts in graph visualization

--- plot_graph.py
+++ plot_graph.py
@@ -3,14 +3,22 @@ from graphviz import Digraph
 import os
 os.environ["PATH"] += os.pathsep + 'C:/Program Files/graphviz-2.38/bin'
 
-dot = Digraph(comment='The Round Table')
+dot = Digraph(comment='The Causal Model')
+dot.graph_attr['rankdir'] = 'LR'
 #add nodes
 dot.node('I', 'Inflow')
 dot.node('V', 'Volume')
+dot.node('H', 'Height')
+dot.node('P', 'Pressure')
 dot.node('O', 'Outflow')
+dot.node('IV', "Ipos", shape='circle', width='0.05')
 #add edges
-dot.edge('I', 'V', label='I+')
-dot.edge('V', 'O', label='P+')
+dot.edge('I', 'V', label='I+', color='black')
+dot.edge('I', 'IV', len='0.5')
+dot.edge('IV', 'V', len='0.5')
+dot.edge('V', 'H', label='P+')
+dot.edge('H', 'P', label='P+')
+dot.edge('P', 'O', label='P+')
 dot.edge('O', 'V', label="I-")
 #print the graph
 print(dot.source)
