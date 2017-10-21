cf1b73af583280f67452a4540489af48af21c36e
Kaleidophon <Dennis.Ulmer@gmx.de>
2017-10-21 18:29:02 +0200

    networkx rage quit

--- visualization.py
+++ visualization.py
@@ -26,8 +26,11 @@ from graph import init_extra_points_state_graph, init_minimum_viable_state_graph
 
 
 if __name__ == "__main__":
-    graph = nx.DiGraph()
+    graph = nx.Graph()
     state_graph = init_minimum_viable_state_graph()
+
+    _nodes = state_graph.nodes
+    labels = {node: node.replace("|", "\n") for node in _nodes}
 
     for node in state_graph.nodes:
         graph.add_node(node)
@@ -35,8 +38,14 @@ if __name__ == "__main__":
     for start, label, end in state_graph.edges:
         graph.add_edge(start, end)
 
-    pos = nx.random_layout(graph)
-    nx.draw(graph, pos)
+    pos = nx.random_layout(graph, scale=5)
+
+    nx.draw_networkx_nodes(graph, pos, nodelist=_nodes, node_color="b", node_size=2750, alpha=0.5, node_shape="s", linewidths=0)
+    nx.draw_networkx_labels(graph, pos, labels, font_size=12)
+
+    nodes = state_graph.nodes
+    #nx.draw_networkx_labels(graph, pos, {i: node for i, node in zip(range(len(nodes), nodes))}, font_size=16)
 
     # show graph
+    plt.axis("off")
     plt.show()
